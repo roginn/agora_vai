ef333a1c36977f73dbe19db8550a2d0d4078f07f
William Yugue <williamyugue@gmail.com>
2016-05-21 23:00:13 -0300

    Yugue trying to make this work....

--- server/agora_vai.rb
+++ server/agora_vai.rb
@@ -5,6 +5,17 @@ class AgoraVai < Sinatra::Application
     'PEIXE ESTEVE AQUI CARAJO!'
   end
 
+# Handle POST-request (Receive and save the uploaded file)
+post "/upload" do 
+  File.open('uploads/' + params['myfile'][:filename], "w") do |f|
+    f.write(params['myfile'][:tempfile].read)
+  end
+  return "The file was successfully uploaded!"
+end
+
+
+
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
