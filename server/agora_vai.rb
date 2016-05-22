6afa5c2164eb82b72f59f641e3b0c54c7a803b6a
William Yugue <williamyugue@gmail.com>
2016-05-21 23:18:03 -0300

    Yugue trying to make this work....

--- server/agora_vai.rb
+++ server/agora_vai.rb
@@ -1,17 +1,27 @@
 require 'sinatra/base'
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+
 
 class AgoraVai < Sinatra::Application
   get '/' do
     'PEIXE ESTEVE AQUI CARAJO!'
   end
 
+
+ 
+# Handle GET-request (Show the upload form)
+get "/upload" do
+  haml :upload
+end      
+    
 # Handle POST-request (Receive and save the uploaded file)
 post "/upload" do 
-    # File.open('uploads/' + params['myfile'][:filename], "w") do |f|
-    #   f.write(params['myfile'][:tempfile].read)
-    # end
-    # return "The file was successfully uploaded!"
-    return params
+  File.open('uploads/' + params['myfile'][:filename], "w") do |f|
+    f.write(params['myfile'][:tempfile].read)
+  end
+  return "The file was successfully uploaded!"
 end
 
 
