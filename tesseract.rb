3e9a9961da155d4e0f54b54f5000e879d6e5ecba
Thiago Brandão <thiagobrandam@gmail.com>
2016-05-22 11:13:17 -0300

    Updates

--- tesseract.rb
+++ tesseract.rb
@@ -3,27 +3,27 @@ require 'sqlite3'
 db = SQLite3::Database.new "questions.db"
 
 rows = db.execute <<-SQL
-  DROP TABLE IF EXISTS questions;
+  -- DROP TABLE IF EXISTS questions;
 
   CREATE TABLE questions (
-    id int PRIMARY KEY NOT NULL,
+    id integer PRIMARY KEY AUTOINCREMENT,
     value varchar(1000),
     topic varchar(30)
   );
 SQL
 
-files = [] # Dir['*']
+def remove_question_options(text)
+  text.gsub(/a\)[\S\s]*/,'')
+end
+
+files = Dir['scraping/questions/*/**']
 # as per https://gist.github.com/henrik/1967035
 files.each do |file|
-  # topic = ...
+  _, _, topic, _ = file.split('/')
   %x(convert "#{file}" -resize 400% -type Grayscale input.tif)
   %x(tesseract -l por input.tif output)
 
   db.execute(%Q{
     INSERT INTO questions (value, topic) VALUES (?, ?)
   }, [remove_question_options(File.read('output.txt')), topic])
-end
-
-def remove_question_options(text)
-  text.gsub(/a\)[\S\s]*/,'')
 end
