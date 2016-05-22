663ddad3db9e8c252636f35317432402eb01aeb3
Victor Franco <victor@redealumni.com>
2016-05-21 21:14:37 -0300

    Adds accountmaker.rb to /scraping (not quite working yet)

---
+++ scraping/accountmaker.rb
@@ -0,0 +1,53 @@
+require 'watir'
+require 'watir-webdriver'
+
+NEW_ACCOUNT_URL = 'https://www.sprweb.com.br/mod_superpro/index.php?PG=FORM_GRATIS'.freeze
+SUPERPROFESSOR_TITLE = 'SuperPro Web'.freeze
+MAILINATOR_TITLE = 'Mailinator'.freeze
+WAIT_TIMEOUT = 2
+MAIL_TITLE = /\s*Interbits - Superpro Web\s*/
+
+def mailinator_url(inbox)
+  "https://www.mailinator.com/inbox2.jsp?public_to=#{inbox}#/#public_maildirdiv"
+end
+
+nome = 'agoravai_' + SecureRandom.hex[0..5]
+email = nome + '@mailinator.com'
+password = SecureRandom.hex[0..6]
+
+puts "nome = #{nome}"
+puts "email = #{email}"
+puts "password = #{password}"
+
+begin
+  browser = Watir::Browser.new
+  browser.goto NEW_ACCOUNT_URL
+
+  browser.text_field(name: 'NOME').set nome
+  browser.text_field(name: 'EMAIL').set email
+  # browser.text_field(name: 'CELULAR').set ''
+  browser.select_list(name: 'PERFIL').select 'Professor'
+  browser.input(name: 'button', id: 'button2').click
+  browser.alert.ok
+
+  browser.goto mailinator_url(nome)
+  browser.div(id: 'public_maildirdiv').div(text: MAIL_TITLE).wait_until_present WAIT_TIMEOUT
+  puts "Timed out in #{WAIT_TIMEOUT}s waiting for email"
+  browser.div(id: 'public_maildirdiv').div(text: MAIL_TITLE).click
+  create_password_link = browser.element(:css, '[href^="http://click1.clickrouter.com/redirect?token="]').href
+  puts "create_password_link = #{create_password_link}"
+
+  browser.goto create_password_link
+  browser.input(name: 'PASSWD').set nome
+  browser.input(name: 'CONF_PASSWD').set nome
+  browser.input(name: 'entrar').click
+ensure
+  yaml = {
+    'name' => nome || '',
+    'email' => email || '',
+    'create_password_link' => create_password_link || '',
+    'password' => password || ''
+  }.to_yaml
+
+  File.open("account_#{nome}", 'w') { |file| file << yaml }
+end
